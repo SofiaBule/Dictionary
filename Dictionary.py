853348f824a213c9d8465ae301e70f202707fa78
SofiaBule <146926716+SofiaBule@users.noreply.github.com>
2024-03-17 10:33:52 +0100

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+birds = {
+    "sparrow": "brown",
+    "parrot": "green",
+    "owl": "gray",
+}
+
+# Accessing bird information
+print("Color of sparrow:", birds["sparrow"])
+print("Color of parrot:", birds["parrot"])
